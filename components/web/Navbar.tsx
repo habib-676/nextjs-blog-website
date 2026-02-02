e1cd20b3aef870a270d7994976ef4f8e12bbe23c
habib-676 <rahman.habibur2421k@gmail.com>
2026-02-02 23:02:00 +0600

    feat: integrate Sonner for toast notifications and add Toaster component

--- components/web/Navbar.tsx
+++ components/web/Navbar.tsx
@@ -5,10 +5,12 @@ import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
 import { useConvexAuth } from "convex/react";
 import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
   const { isAuthenticated, isLoading } = useConvexAuth();
-
+  const router = useRouter();
   return (
     <nav className="w-full py-5 flex items-center justify-between ">
       <div className="flex items-center gap-8">
@@ -33,7 +35,23 @@ export default function Navbar() {
       </div>
       <div className="flex items-center gap-2">
         {isLoading ? null : isAuthenticated ? (
-          <Button onClick={() => authClient.signOut({})}>Logout</Button>
+          <Button
+            onClick={() =>
+              authClient.signOut({
+                fetchOptions: {
+                  onSuccess: () => {
+                    toast.success("Logged out successfully");
+                    router.push("/");
+                  },
+                  onError: (error) => {
+                    toast.error(error.error.message);
+                  },
+                },
+              })
+            }
+          >
+            Logout
+          </Button>
         ) : (
           <>
             <Link className={buttonVariants()} href="/auth/signup">
